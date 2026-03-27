5fb52c1a0a1ac7b31ec8e9ad59e3e3d6a29cf2d2
Ggeshechka <ix.reylk@gmail.com>
2026-03-27 19:23:14 +0500

    first commit

---
+++ src-tauri/gen/android/app/src/main/java/com/pro100/tauriapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pro100.tauriapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
